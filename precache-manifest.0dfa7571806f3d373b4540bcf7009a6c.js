b2290c0a667107b97332a14d5ac943eca2d9184b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-21 23:02:35 +0000

    Deploy neighbor-app/website to github.com/neighbor-app/website.git:gh-pages

---
+++ precache-manifest.0dfa7571806f3d373b4540bcf7009a6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545dfea42c1898a75571b74059fb8279",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "e59d36ce47154bd15c03",
+    "url": "/website/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a096cc74ff52d14e568e",
+    "url": "/website/static/js/2.2daa1ece.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/website/static/js/2.2daa1ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e59d36ce47154bd15c03",
+    "url": "/website/static/js/main.5a440095.chunk.js"
+  },
+  {
+    "revision": "ceeca9d7a592b35c57db",
+    "url": "/website/static/js/runtime-main.753c8fe7.js"
+  },
+  {
+    "revision": "89405429a365906ab301f3f2e18e1938",
+    "url": "/website/static/media/home.89405429.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dd2143bfae508c5dbb0f1938f67f4dc8.js"
+  "/website/precache-manifest.0dfa7571806f3d373b4540bcf7009a6c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
