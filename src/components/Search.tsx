070d5bbfbc468bb14f4b1c904f05d31456057cd1
andkre <a.kreienmeier@gmail.com>
2020-03-21 19:56:13 +0100

    Search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,43 +1,92 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import { TextField } from '@material-ui/core';
+import { TextField, Grid, Button, Divider } from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import DateFnsUtils from '@date-io/date-fns';
+
 import {
     MuiPickersUtilsProvider,
     KeyboardTimePicker,
     KeyboardDatePicker,
 
   } from '@material-ui/pickers';
+import Header from './Header';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 interface ComponentProps {}
 interface ComponentState {}
 
+
+
 class Search extends React.Component<ComponentProps, ComponentState> {
-    selectedDate: Date = new Date('2014-08-18T21:11:54');
-    setSelectedDate: Date= new Date('2014-08-18T21:11:54'); 
+
+    selectedDate: Date = new Date();
+     
+   
 
     handleDateChange( date :any ) {
+      console.log(date)
+      console.log(this);
         this.selectedDate = date;
+
+        console.log(this.selectedDate);
     }
+
+    stores = [
+      'REWE',
+      'Aldi',
+      'Lidl',
+      'Netto',
+      'Fressnapf',
+      'Edeka'
+    ];
+    
+    searchContainer = {
+      width:"100%",
+      maxWidth:"500px"
+    };
 
     render() {
         return (
             <>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <KeyboardDatePicker
-          disableToolbar
-          variant="inline"
-          format="MM/dd/yyyy"
-          margin="normal"
-          id="date-picker-inline"
-          label="Date picker inline"
-          value={this.selectedDate}
-          onChange={this.handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        />
-                </MuiPickersUtilsProvider>
+            <Header/>
+            <div style={this.searchContainer}>
+            <Grid container direction="column" spacing={4} justify="center" alignItems="stretch">
+              <Grid item>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                      <KeyboardDatePicker
+                    disablePast={true}
+                    variant="inline"
+                    format="dd/MM/yyyy"
+                    margin="normal"
+                    id="date-picker-inline"
+                    label="Wann brauchst du deinen Einkauf"
+                    value={this.selectedDate}
+                    onChange={this.handleDateChange.bind(this)}
+                    KeyboardButtonProps={{
+                      'aria-label': 'change date',
+                    }}
+                  />
+              </MuiPickersUtilsProvider>
+              </Grid>
+              <Grid item>
+              <Autocomplete
+         
+                  options={ this.stores }
+                  getOptionLabel={(option:string) => option}
+         
+                  renderInput={ (params:any) => <TextField {...params} label="Aus welchem Laden brauchst du etwas?" variant="outlined" />}
+                />
+                </Grid>
+                <Divider/>
+                  <Grid item container direction="row"  justify="center" alignItems="center">
+                    <Grid item >
+                    <Link to="/needer"><Button variant="contained" color="primary">Suchen</Button></Link>
+                    </Grid>
+                  </Grid>
+                </Grid> 
+                </div>
             </>
         );
     }
